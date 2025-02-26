441359d9746cdf61c3127ca715bb43dba8f8478b
GABRIEL ANGEL SANCHEZ VELAZQUEZ <gasv1988@gmail.com>
2025-02-25 21:25:59 -0600

    Proyectos
    proyectos

---
+++ EjemploClase2.py
@@ -0,0 +1,5 @@
+print(type("Hola mundo"))
+peso=68/2
+print(type(peso),peso)
+altura=1.68
+print(type(altura),altura)
---
+++ IMC.py
@@ -0,0 +1,14 @@
+# print("Hola Mundo") #primera prueba
+# print = input ("¿Cual es tu nombre?")
+
+print ("Hola Mundo")
+nombre1 = input("¿Cual es tu nombre o nombres? \n")
+nombre2 = input("¿Cuel es tu primer apellido? \n") 
+nombre3 = input("¿Cual es tu segundo apellido? \n") 
+# print = ("Hola + nombre1 + nombre2 + nombre3") # error
+# nombre_completo = nombre1 + nombre2 + nombre3 # error
+print (type("Hola + {nombre_completo}"))
+
+edad = input("¿Cuantos años tienes?")
+print(type(edad))
+print(f"{nombre} tiene {edad} años")
